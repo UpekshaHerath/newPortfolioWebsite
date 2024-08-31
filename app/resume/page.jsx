3f600ee2d18ce55066e4a657545a6d57065df89f
UpekshaHerath <upekshadilshan000@gmail.com>
2024-09-01 02:35:05 +0530

    feat: created the resume part

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -111,76 +111,46 @@ const skills = {
     "Mainly my technical skills and interpersonal skills have been listed below.",
   skillList: [
     {
-      title: "Technical Skills",
-      skills: [
-        {
-          icon: <FaReact />,
-          name: "react.js",
-        },
-        {
-          icon: <SiNextdotjs />,
-          name: "next.js",
-        },
-        {
-          icon: <SiTailwindcss />,
-          name: "tailwind.css",
-        },
-        {
-          icon: <SiSpringboot />,
-          name: "spring boot",
-        },
-        {
-          icon: <FaNodeJs />,
-          name: "node.js",
-        },
-        {
-          icon: <FaFigma />,
-          name: "figma",
-        },
-
-        {
-          icon: <FaHtml5 />,
-          name: "html 5",
-        },
-        {
-          icon: <FaCss3 />,
-          name: "css 3",
-        },
-        {
-          icon: <FaJs />,
-          name: "javascript",
-        },
-        {
-          icon: <SiCanva />,
-          name: "canva",
-        },
-      ],
+      icon: <FaReact />,
+      name: "react.js",
     },
     {
-      title: "Interpersonal Skills",
-      skills: [
-        {
-          icon: <TbMathSymbols />,
-          name: "Problem Solving"
-        },
-        {
-          icon: <RiTeamLine />,
-          name: "Leadership"
-        },
-        {
-          icon: <GiPublicSpeaker />,
-          name: "Communication"
-        },
-        {
-          icon: <FaReact />,
-          name: "Creativity"
-        },
-        {
-          icon: <FaReact />,
-          name: "Co-ordination"
-        }
-      ],
+      icon: <SiNextdotjs />,
+      name: "next.js",
     },
+    {
+      icon: <SiSpringboot />,
+      name: "spring boot",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "tailwind.css",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "node.js",
+    },
+    {
+      icon: <FaFigma />,
+      name: "figma",
+    },
+
+    {
+      icon: <FaHtml5 />,
+      name: "html 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "css 3",
+    },
+    {
+      icon: <FaJs />,
+      name: "javascript",
+    },
+    {
+      icon: <SiCanva />,
+      name: "canva",
+    }
   ],
 };
 
@@ -323,25 +293,25 @@ const Resume = () => {
               value="about"
               className="w-full text-center xl:text-left"
             >
-              <div className="flex flex-col gap-[30px]">
-                <h3 className="text-4xl font-bold">{about.title}</h3>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
-                  {about.description}
-                </p>
-                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
-                  {about.info.map((item, index) => {
-                    return (
-                      <li
-                        key={index}
-                        className="flex items-center justify-center xl:justify-start gap-4"
-                      >
-                        <span className="text-white/60">{item.fieldName}</span>
-                        <span className="text-xl">{item.fieldValue}</span>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+                <div className="flex flex-col gap-[30px]">
+                  <h3 className="text-4xl font-bold">{about.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {about.description}
+                  </p>
+                  <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                    {about.info.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="flex items-center justify-center xl:justify-start gap-4"
+                        >
+                          <span className="text-white/60">{item.fieldName}</span>
+                          <span className="text-xl">{item.fieldValue}</span>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
             </TabsContent>
           </div>
         </Tabs>
